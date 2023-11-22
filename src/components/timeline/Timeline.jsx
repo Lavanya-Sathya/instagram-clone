8a4526f4419c12181ca51d3f4d8e26da82493762
Lavanya-Sathya <sathyalavanya5@gmail.com>
2023-11-22 23:42:06 +0530

    search-explore-component-added

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -12,14 +12,16 @@ import {
   orderBy,
 } from "firebase/firestore";
 function Timeline() {
+  // variables to keep track of loading, error and store post details
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  // Load the post details from firebase
   useEffect(() => {
     const fetchData = async () => {
       try {
         // const q = query(collection(db, "posts"));
-        const snapshot = await onSnapshot(
+        const unsubscribe = await onSnapshot(
           query(collection(db, "posts"), orderBy("timestamp", "desc")),
           (querySnapshot) => {
             const postsArray = [];
@@ -27,10 +29,14 @@ function Timeline() {
               postsArray.push({ id: doc.id, data: doc.data() });
             });
             setPosts(postsArray);
+            setIsLoading(false);
           }
         );
+        return () => unsubscribe();
       } catch (error) {
         console.error("Error fetching data:", error);
+        setError(true);
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -38,18 +44,22 @@ function Timeline() {
   return (
     <div className="timeline">
       <div className="timelineLeft mt-1">
-        <div className="postTimeline mb-5">
-          {posts.map((post) => (
-            <Post
-              key={post.id}
-              user={post.data.user}
-              postImage={post.data.postImage}
-              likes={post.data.likes}
-              timestamp={post.data.timestamp}
-              caption={post.data.caption}
-            />
-          ))}
-        </div>
+        {isLoading && <p className="text-center h2 m5">Loading...</p>}
+        {error && <p className="text-center h2 m5">Error loading data</p>}
+        {!isLoading && !error && (
+          <div className="postTimeline mb-5">
+            {posts.map((post) => (
+              <Post
+                key={post.id}
+                user={post.data.user}
+                postImage={post.data.postImage}
+                likes={post.data.likes}
+                timestamp={post.data.timestamp}
+                caption={post.data.caption}
+              />
+            ))}
+          </div>
+        )}
       </div>
       <div className="timelineRight">
         <div className="sugesstionTime d-flex align-items-center justify-content-between p-2 mb-3">
