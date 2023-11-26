784a6c27b0515ffc4dc2ab65c755f0187490f314
Lavanya-Sathya <sathyalavanya5@gmail.com>
2023-11-26 23:55:28 +0530

    reels-dynamically-uploaded

--- src/components/timeline/Reels/Reels.jsx
+++ src/components/timeline/Reels/Reels.jsx
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from "react";
 import "./Reels.css";
 import { db } from "../../Firebase/Firebase";
+import {
+  collection,
+  onSnapshot,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
 import userProfile from "@/image/img.jpg";
-import video1 from "@/image/video/video1.mp4";
-import video2 from "@/image/video/video2.mp4";
 
 import ReelVideo from "./ReelVideo";
 function Reels() {
   const [profileReel, setProfileReel] = useState(null);
   useEffect(() => {
     // Fetch the current user's post data from the firebase
-    const userId = JSON.parse(sessionStorage.getItem("Token"));
     const fetchUserData = async () => {
       try {
         const snapshot = await onSnapshot(
           query(
             collection(db, "posts"),
-            where("uid", "==", userId),
             where("type", "==", "video"),
             orderBy("timestamp", "desc")
           ),
           (querySnapshot) => {
             const postsArray = [];
             querySnapshot.forEach((doc) => {
+              console.log(doc.id, " => ", doc.data());
               postsArray.push({ id: doc.id, data: doc.data() });
             });
             setProfileReel(postsArray);
@@ -38,24 +42,19 @@ function Reels() {
     <div className="reelsMainContainer">
       <div className="reelContainer">
         <div className="reelVideos">
-          <ReelVideo
-            channel="escapeTime"
-            avatarSrc={userProfile}
-            song="music name"
-            url={video1}
-            likes="200"
-            comments="20"
-            caption="This is the caption for the post. Caption goes here"
-          />
-          <ReelVideo
-            channel="escapeTime"
-            avatarSrc={userProfile}
-            song="test song"
-            url={video2}
-            likes="140"
-            comments="14"
-            caption="This is the caption for the post"
-          />
+          {profileReel &&
+            profileReel.map((post) => (
+              <ReelVideo
+                key={post.id}
+                channel={post.data.user}
+                avatarSrc={userProfile}
+                song="music name"
+                url={post.data.postImage}
+                likes="200"
+                comments="20"
+                caption={post.data.caption}
+              />
+            ))}
         </div>
       </div>
     </div>
