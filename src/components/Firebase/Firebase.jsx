37f2b9816d6efdb4a390c54b5b747c7640d03d6f
Lavanya-Sathya <sathyalavanya5@gmail.com>
2023-11-27 19:25:17 +0530

    gitignore-updated

--- src/components/Firebase/Firebase.jsx
+++ src/components/Firebase/Firebase.jsx
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyA_-5Ddxd97ZLFg1dhGgbkTUnu7hZvR4Mk",
-  authDomain: "instagram-clone-2f3bd.firebaseapp.com",
-  projectId: "instagram-clone-2f3bd",
-  storageBucket: "instagram-clone-2f3bd.appspot.com",
-  messagingSenderId: "815670129637",
-  appId: "1:815670129637:web:e22e80e9a32ce883b3f50c",
-  measurementId: "G-NLFC40DGDX",
+  apiKey: "Api_Key",
+  authDomain: "Auth_Domain",
+  projectId: "project_id",
+  storageBucket: "storage_bucket",
+  messagingSenderId: "message_Sender",
+  appId: "app_id",
+  measurementId: "measurementId",
 });
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
